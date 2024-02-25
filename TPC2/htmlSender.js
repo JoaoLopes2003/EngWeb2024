168b5bf4fa0561db4e25fa625aaf3b00ca13242a
JoaoLopes2003 <joaommlopes2003@gmail.com>
2024-02-25 23:06:42 +0000

    Alteração na página do erro 404

--- TPC2/htmlSender.js
+++ TPC2/htmlSender.js
@@ -3,28 +3,28 @@ var fs = require('fs')
 var url = require('url')
 
 http.createServer(function (req, res) {
-    var regex = /^c\d+$/
     var q = url.parse(req.url, true);
     var path = q.pathname.substring(1);
-    console.log("query: " + q);
-    console.log("path: " + path);
-    console.log("pathname: " + q.pathname.substring(1));
     if(path=="") {
         fs.readFile("html/index.html", function (erro, dados) {
             res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
             res.write(dados);
             res.end();
         })
-    } else if (regex.test(path)) {
-        fs.readFile("html" + q.pathname + ".html", function (erro, dados) {
-            res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-            res.write(dados);
-            res.end();
-        })
-    } else {
-        res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'})
-        res.write('<p>Erro: Page not found.</p>')
-        res.write('<pre>' + q.pathname + '</pre>')
-        res.end()
+    } else{
+        fs.access("html" + q.pathname + ".html", fs.constants.F_OK, (err) => {
+            if (!err) {
+                fs.readFile("html" + q.pathname + ".html", function (erro, dados) {
+                    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+                    res.write(dados);
+                    res.end();
+                })
+            } else {
+                res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'})
+                res.write('<h3>404: Page not found.</h3>')
+                res.write('<pre>Page asked: ' + q.pathname + '</pre>')
+                res.end()
+            }
+        });
     }
 }).listen(7777);
